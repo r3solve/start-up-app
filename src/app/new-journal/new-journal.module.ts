238603bfc0d1e6b4975881a6a3eb7d893c085a85
r3sovle <yelpoeayahaya@gmail.com>
2024-05-18 09:49:20 +0000

    done with journal

--- src/app/new-journal/new-journal.module.ts
+++ src/app/new-journal/new-journal.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { NewJournalPageRoutingModule } from './new-journal-routing.module';
 
@@ -15,6 +16,7 @@ import { NewJournalPage } from './new-journal.page';
     IonicModule,
     NewJournalPageRoutingModule
   ],
-  declarations: [NewJournalPage]
+  declarations: [NewJournalPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NewJournalPageModule {}
